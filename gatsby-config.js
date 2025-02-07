0ae75af703517f3765785fea3a4438a182154b7e
nagykorte <nicolasatilamatray@gmail.com>
2025-02-07 20:45:50 -0300

    pt

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `chichin_ardido`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
