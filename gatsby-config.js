6c9a34adee15ffa48470fd166d9b76f45e68d45d
nagykorte <nicolasatilamatray@gmail.com>
2025-02-07 21:00:45 -0300

    sad

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+const adapter = require("gatsby-adapter-netlify");
+
 module.exports = {
   siteMetadata: {
     title: `chichin_ardido`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false
+  })
 }
